992424e93d4e260e52c8e6e4bb97bbdcd4a92f53
Hunter Barrington <hunter@ftwdev.com>
2013-05-09 12:22:04 -0400

    this is my first commit for the blog. It is great. I love gitgit commit -m

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'bb7f52a057fbbca63674d6ac81da42afdae3992679f2fa216ac874494a54694ef575c448c336ff008c7ae3d4a96e5308050b3ef2b3e8ecccc8a145d40a387c1a'
